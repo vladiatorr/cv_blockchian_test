b16c3b4d9ce00990e868f6e99bb0f668f98439ee
vladiatorr <31032721+vladiatorr@users.noreply.github.com>
2017-09-20 11:14:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ window.web3 = new Web3(web3.currentProvider);
 abi = JSON.parse('[{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"totalVotesFor","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"validCandidate","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"bytes32"}],"name":"votesReceived","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"candidateList","outputs":[{"name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"voteForCandidate","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[{"name":"candidateNames","type":"bytes32[]"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"}]')
 VotingContract = web3.eth.contract(abi);
 // In your nodejs console, execute contractInstance.address to get the address at which the contract is deployed and change the line below to use your deployed address
-contractInstance = VotingContract.at('0x9e2b12050Cd579b1E0F6240E873d0375FdB30ba2');
+contractInstance = VotingContract.at('0x987c626359dd452d40fdbea1e29e745dcf9f485b');
 candidates = {"Nick": "candidate-1", "Sam": "candidate-2", "Bob": "candidate-3"}
 
 function voteForCandidate() {
