3546c763afe4264e8fb822c1a3064896ff059877
vladiatorr <31032721+vladiatorr@users.noreply.github.com>
2017-09-21 17:02:56 +0200

    t

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
-window.web3 = new Web3(web3.currentProvider);
+const web3 = new Web3(new Web3.providers.HttpProvider("https://ropsten.infura.io/TOKEN"));
+
+//web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 abi = JSON.parse('[{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"totalVotesFor","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"validCandidate","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"bytes32"}],"name":"votesReceived","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"candidateList","outputs":[{"name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"voteForCandidate","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[{"name":"candidateNames","type":"bytes32[]"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"}]')
 VotingContract = web3.eth.contract(abi);
-// In your nodejs console, execute contract.address to get the address at which the contract is deployed and change the line below to use your deployed address
-var address = {
-    "3" : "0x987c626359dd452d40fdbea1e29e745dcf9f485b" // Ropsten
-  }
-var contract = web3.eth.contract(abi).at(address["3"]);
-
-candidates = {"Nick": "candidate-1", "Sam": "candidate-2", "Bob": "candidate-3"}
+// In your nodejs console, execute contractInstance.address to get the address at which the contract is deployed and change the line below to use your deployed address
+contractInstance = VotingContract.at('0x987c626359dd452d40fdbea1e29e745dcf9f485b');
+candidates = {"Rama": "candidate-1", "Nick": "candidate-2", "Jose": "candidate-3"}
 
 function voteForCandidate() {
   candidateName = $("#candidate").val();
-  contract.voteForCandidate(candidateName, {from: web3.eth.accounts[0]}, function() {
+  contractInstance.voteForCandidate(candidateName, {from: web3.eth.accounts[0]}, function() {
     let div_id = candidates[candidateName];
-    $("#" + div_id).html(contract.totalVotesFor.call(candidateName).toString());
+    $("#" + div_id).html(contractInstance.totalVotesFor.call(candidateName).toString());
   });
 }
 
@@ -21,7 +19,7 @@ $(document).ready(function() {
   candidateNames = Object.keys(candidates);
   for (var i = 0; i < candidateNames.length; i++) {
     let name = candidateNames[i];
-    let val = contract.totalVotesFor.call(name).toString()
+    let val = contractInstance.totalVotesFor.call(name).toString()
     $("#" + candidates[name]).html(val);
   }
 });
