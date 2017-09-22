9917c0d1f02cc622b73fe0e41027c151c82c5ef5
vladiatorr <31032721+vladiatorr@users.noreply.github.com>
2017-09-22 10:03:48 +0200

    t

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 //const web3 = new Web3(new Web3.providers.HttpProvider("https://ropsten.infura.io/TOKEN"));
 
+window.web3 = new Web3(web3.currentProvider);
+
 abi = JSON.parse('[{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"totalVotesFor","outputs":[{"name":"","type":"uint8"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"validCandidate","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"bytes32"}],"name":"votesReceived","outputs":[{"name":"","type":"uint8"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"x","type":"bytes32"}],"name":"bytes32ToString","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"candidateList","outputs":[{"name":"","type":"bytes32"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"voteForCandidate","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"contractOwner","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"inputs":[{"name":"candidateNames","type":"bytes32[]"}],"payable":false,"type":"constructor"}]')
-VotingContract = web3.eth.contract(abi);
+contract = web3.eth.contract(abi);
 // In your nodejs console, execute contract.address to get the address at which the contract is deployed and change the line below to use your deployed address
 
   var address = {
     "3" : "0x987c626359dd452d40fdbea1e29e745dcf9f485b" // Ropsten
   }
 
-var current_network = web3.version.network;
-var contract = web3.eth.contract(abi).at(address[current_network]);
+//var current_network = web3.version.network;
+//var contract = web3.eth.contract(abi).at(address[current_network]);
 
- // var contract = web3.eth.contract(abi).at(address["3"]);
+  var contract = web3.eth.contract(abi).at(address["3"]);
 
 //contract = VotingContract.at('0x987c626359dd452d40fdbea1e29e745dcf9f485b');
 candidates = {"Rama": "candidate-1", "Nick": "candidate-2", "Jose": "candidate-3"}
