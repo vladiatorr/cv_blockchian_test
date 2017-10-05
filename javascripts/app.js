c9594a16ed83f75210da1522922d572b739cfc42
vladiatorr <31032721+vladiatorr@users.noreply.github.com>
2017-10-05 21:08:00 +0200

    please!..

--- javascripts/app.js
+++ javascripts/app.js
@@ -46,15 +46,9 @@ window.voteForCandidate = function(candidate) {
 }
 
 $( document ).ready(function() {
-  if (typeof web3 !== 'undefined') {
-    console.warn("Using web3 detected from external source like Metamask")
-    // Use Mist/MetaMask's provider
-    window.web3 = new Web3(web3.currentProvider);
-  } else {
-    console.warn("No web3 detected. Falling back to http://localhost:8545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
-    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-    window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-  }
+
+  window.web3 = new Web3(web3.currentProvider);
+
 
   Voting.setProvider(web3.currentProvider);
   let candidateNames = Object.keys(candidates);
